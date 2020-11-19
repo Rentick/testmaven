8fd704dd5592e82ad80cdb30aeb4ce460f8bcfc3
Renat Khaybullin <zabzari@gmail.com>
2020-11-19 13:30:39 +0300

    added jange

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -12,4 +12,12 @@ class StatisticServiceTest {
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
+    @Test
+    void findMax1() {
+        StatisticService service = new StatisticService();
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 10, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
